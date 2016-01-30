6df31afd00210b7697e6c9f71ed940a18d2319a7
Eston Karumbi <eston.karumbi@andela.com>
2016-01-31 00:19:00 +0300

    Implemented tests for surname and firstName attribute getters in class Member

---
+++ test/com/andela/bookclub/models/MemberTest.java
@@ -0,0 +1,58 @@
+package com.andela.bookclub.models;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.mockito.Mockito.mock;
+
+
+public class MemberTest {
+
+    // Private variable to use for testing
+
+    Member member;
+
+    @Before
+    public void beforeTestGetSurname() {
+        member = mock(Member.class, Mockito.CALLS_REAL_METHODS);
+        member.setSurname("Smith");
+    }
+
+    @Test
+    public void testGetSurname() throws Exception {
+        String memberSurname = member.getSurname();
+        assertEquals("Smith", memberSurname);
+        assertNotSame("smith", memberSurname);
+    }
+
+    @Before
+    public void beforeTestGetFirstName() {
+        member = mock(Member.class, Mockito.CALLS_REAL_METHODS);
+        member.setFirstName("John");
+    }
+
+    @Test
+    public void testGetFirstName() throws Exception {
+        String memberFirstName = member.getFirstName();
+        assertEquals("John", memberFirstName);
+        assertNotSame("john", memberFirstName);
+    }
+
+    @Test
+    public void testGetMiddleName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNationalIdNumber() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAge() throws Exception {
+
+    }
+}

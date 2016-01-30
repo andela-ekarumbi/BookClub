27d2e1e1ac90395e69724a9351ae475c2dfb6cce
Eston Karumbi <eston.karumbi@andela.com>
2016-01-30 20:00:48 +0300

    Created class Model

---
+++ src/com/andela/bookclub/models/Model.java
@@ -0,0 +1,16 @@
+package com.andela.bookclub.models;
+
+/*
+*  The superclass for all model classes
+*/
+public class Model {
+    protected String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}

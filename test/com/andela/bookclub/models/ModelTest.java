458b6d6106bfd5072280f494a8f8af118bd4b9d0
Eston Karumbi <eston.karumbi@andela.com>
2016-01-30 23:16:57 +0300

    Revert "Created class StaffMember and its accompanying test class"
    This reverts commit 8891214f8e38dcf4f51e7e079b44e60a03ba7f09.

---
+++ test/com/andela/bookclub/models/ModelTest.java
@@ -0,0 +1,19 @@
+package com.andela.bookclub.models;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by emkarumbi on 30/01/2016.
+ */
+public class ModelTest {
+
+    @org.junit.Test
+    public void testGetId() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetId() throws Exception {
+
+    }
+}

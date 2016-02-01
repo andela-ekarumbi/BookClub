2f7e7c402c632f365a971a742adef947a98c591f
Eston Karumbi <eston.karumbi@andela.com>
2016-02-01 22:32:45 +0300

    Created class StudentMember

---
+++ src/com/andela/bookclub/models/StudentMember.java
@@ -0,0 +1,13 @@
+package com.andela.bookclub.models;
+
+public class StudentMember extends Member {
+    private String school;
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+}

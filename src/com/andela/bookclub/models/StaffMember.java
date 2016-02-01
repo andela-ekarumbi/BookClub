96d158aadda7d2f671acf942edede02570d36dcb
Eston Karumbi <eston.karumbi@andela.com>
2016-02-01 20:27:53 +0300

    Created class StaffMember

---
+++ src/com/andela/bookclub/models/StaffMember.java
@@ -0,0 +1,13 @@
+package com.andela.bookclub.models;
+
+public class StaffMember extends Member {
+    private double salaryValue;
+
+    public double getSalaryValue() {
+        return salaryValue;
+    }
+
+    public void setSalaryValue(double salaryValue) {
+        this.salaryValue = salaryValue;
+    }
+}

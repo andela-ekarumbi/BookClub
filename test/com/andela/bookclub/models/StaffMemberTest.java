aebc078e3c185a7d0e8bce7006101b0e9232aa6b
Eston Karumbi <eston.karumbi@andela.com>
2016-02-01 20:34:13 +0300

    Implemented tests for getSalaryValue in class StaffMember

--- test/com/andela/bookclub/models/StaffMemberTest.java
+++ test/com/andela/bookclub/models/StaffMemberTest.java
@@ -1,13 +1,24 @@
 package com.andela.bookclub.models;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
 public class StaffMemberTest {
 
+    private StaffMember staffMember = new StaffMember();
+
+    @Before
+    public void beforeTestGetSalaryValue() {
+        staffMember.setSalaryValue(10000d);
+    }
+
     @Test
     public void testGetSalaryValue() throws Exception {
-
+        double staffMemberSalary = staffMember.getSalaryValue();
+        assertNotSame(0.0, staffMemberSalary);
+        assertEquals(10000d, staffMemberSalary, 0.0);
     }
 }

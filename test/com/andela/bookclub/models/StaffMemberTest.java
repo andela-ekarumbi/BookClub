9de68c7f0961a9273c294dd7c6ea0c85ed68bf66
Eston Karumbi <eston.karumbi@andela.com>
2016-02-01 20:29:12 +0300

    Created test suite for class StaffMember

---
+++ test/com/andela/bookclub/models/StaffMemberTest.java
@@ -0,0 +1,13 @@
+package com.andela.bookclub.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StaffMemberTest {
+
+    @Test
+    public void testGetSalaryValue() throws Exception {
+
+    }
+}

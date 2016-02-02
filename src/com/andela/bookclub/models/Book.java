c654c642c1df55e4adef8b5117637e8e3fa30fd2
Eston Karumbi <eston.karumbi@andela.com>
2016-02-02 16:02:09 +0300

    Created class Book

---
+++ src/com/andela/bookclub/models/Book.java
@@ -0,0 +1,13 @@
+package com.andela.bookclub.models;
+
+public class Book extends Model {
+    // Private attributes
+
+    private String title;
+
+    private String isbn;
+
+    private String authorName;
+
+    private String synopsis;
+}

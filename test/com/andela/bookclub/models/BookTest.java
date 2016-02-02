3fc15b6ce3b26fb760bd59904030300abc980736
Eston Karumbi <eston.karumbi@andela.com>
2016-02-02 16:43:51 +0300

    Created test suite for class Book

---
+++ test/com/andela/bookclub/models/BookTest.java
@@ -0,0 +1,28 @@
+package com.andela.bookclub.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookTest {
+
+    @Test
+    public void testGetTitle() throws Exception {
+
+    }
+
+    @Test
+    public void testGetIsbn() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAuthorName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSynopsis() throws Exception {
+
+    }
+}

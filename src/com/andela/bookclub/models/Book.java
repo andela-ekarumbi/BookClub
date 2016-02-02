ab8068c7c7948e730d48bdfef0927f7afae06f74
Eston Karumbi <eston.karumbi@andela.com>
2016-02-02 16:07:30 +0300

    Added getters and setters to class Book

--- src/com/andela/bookclub/models/Book.java
+++ src/com/andela/bookclub/models/Book.java
@@ -10,4 +10,38 @@ public class Book extends Model {
     private String authorName;
 
     private String synopsis;
+
+    // Getters snd setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
 }

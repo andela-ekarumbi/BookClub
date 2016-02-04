3d83995bd20aacf8be8de3212105ebd7fff85cad
Eston Karumbi <eston.karumbi@andela.com>
2016-02-04 11:57:09 +0300

    Implemented tests for getRequestedBook in class BookRequest

--- test/com/andela/bookclub/models/BookRequestTest.java
+++ test/com/andela/bookclub/models/BookRequestTest.java
@@ -1,14 +1,33 @@
 package com.andela.bookclub.models;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Date;
+import java.util.Calendar;
 
 import static org.junit.Assert.*;
 
 public class BookRequestTest {
 
+    // Private variables to use during testing
+
+    private BookRequest bookRequest = new BookRequest();
+    private Book bookToRequest = new Book();
+    private Calendar calendar;
+    private Date bookRequestedDate = new Date();
+
+    @Before
+    public void beforeTestGetRequestedBook() {
+        bookToRequest.setTitle("Treasure Island");
+        bookRequest.setRequestedBook(bookToRequest);
+    }
+
     @Test
     public void testGetRequestedBook() throws Exception {
-
+        Book requestedBook = bookRequest.getRequestedBook();
+        assertNotSame(null, requestedBook);
+        assertEquals(bookToRequest, requestedBook);
     }
 
     @Test

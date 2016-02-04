def5e1b66c80872862e0c08d8ddde839e3aee3ad
Eston Karumbi <eston.karumbi@andela.com>
2016-02-04 11:11:00 +0300

    Created test suite for class BookRequest

---
+++ test/com/andela/bookclub/models/BookRequestTest.java
@@ -0,0 +1,48 @@
+package com.andela.bookclub.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookRequestTest {
+
+    @Test
+    public void testGetBook() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBorrower() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRequestDate() throws Exception {
+
+    }
+
+    @Test
+    public void testIsRequestGranted() throws Exception {
+
+    }
+
+    @Test
+    public void testGetGrantedDate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetGranter() throws Exception {
+
+    }
+
+    @Test
+    public void testGetReturnByDate() throws Exception {
+
+    }
+
+    @Test
+    public void testIsBookReturned() throws Exception {
+
+    }
+}

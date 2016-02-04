1ceb9bc672ab0f8560b4c0f1c067fffef94fe88a
Eston Karumbi <eston.karumbi@andela.com>
2016-02-04 14:20:34 +0300

    Implemented tests for getMember in class LogEntry

--- test/com/andela/bookclub/models/LogEntryTest.java
+++ test/com/andela/bookclub/models/LogEntryTest.java
@@ -1,14 +1,38 @@
 package com.andela.bookclub.models;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
 public class LogEntryTest {
 
+    // Private variable(s) to use during testing
+
+    private LogEntry logEntry = new LogEntry();
+
+    private Member member;
+
+    private Date timeStart;
+
+    private Date timeEnd;
+
+    private StaffMember staffMember;
+
+    @Before
+    public void beforeTestGetMember() {
+        member = Mockito.mock(Member.class, Mockito.CALLS_REAL_METHODS);
+        logEntry.setMember(member);
+    }
+
     @Test
     public void testGetMember() throws Exception {
-
+        Member logMember = logEntry.getMember();
+        assertNotSame(null, logMember);
+        assertEquals(member, logMember);
     }
 
     @Test

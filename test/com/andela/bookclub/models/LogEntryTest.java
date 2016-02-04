19650ab2febfb93a39c191205051e573d6acad3b
Eston Karumbi <eston.karumbi@andela.com>
2016-02-04 14:11:17 +0300

    Created test suite for class LogEntry

---
+++ test/com/andela/bookclub/models/LogEntryTest.java
@@ -0,0 +1,33 @@
+package com.andela.bookclub.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LogEntryTest {
+
+    @Test
+    public void testGetMember() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEntryDescription() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTimeStart() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTimeEnd() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLoggedBy() throws Exception {
+
+    }
+}

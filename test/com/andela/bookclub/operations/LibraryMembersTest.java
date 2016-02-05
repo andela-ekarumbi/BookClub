87853c39304946ae820304447b4b4aa03f85be19
Eston Karumbi <eston.karumbi@andela.com>
2016-02-05 16:15:27 +0300

    Implemented failing tests for addNewMember in class LibraryMember

--- test/com/andela/bookclub/operations/LibraryMembersTest.java
+++ test/com/andela/bookclub/operations/LibraryMembersTest.java
@@ -1,14 +1,34 @@
 package com.andela.bookclub.operations;
 
+import com.andela.bookclub.models.Member;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class LibraryMembersTest {
 
+    // Private variables to use in testing
+
+    private LibraryMembers libraryMembers = new LibraryMembers();
+
+    private Member testMember1;
+
+    private Member testMember2;
+
+    private Member getTestMember3;
+
+    // Test methods
+
+    @Before
+    public void beforeTestAddNewMember() {
+        testMember1 = Mockito.mock(Member.class, Mockito.CALLS_REAL_METHODS);
+    }
+
     @Test
     public void testAddNewMember() throws Exception {
-
+        assertTrue(libraryMembers.addNewMember(testMember1));
     }
 
     @Test

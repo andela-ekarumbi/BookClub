8ff2487c95dea5525c6fb250e9a87a1dce49653f
Eston Karumbi <eston.karumbi@andela.com>
2016-02-05 15:50:11 +0300

    Created test suite for class LibraryManager

---
+++ test/com/andela/bookclub/operations/LibraryMembersTest.java
@@ -0,0 +1,33 @@
+package com.andela.bookclub.operations;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LibraryMembersTest {
+
+    @Test
+    public void testAddNewMember() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllMembers() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMemberById() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateMemberDetails() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteMember() throws Exception {
+
+    }
+}

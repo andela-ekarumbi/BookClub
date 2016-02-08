7c59cd756b272acf064508c444c25d01ccb43ec2
Eston Karumbi <eston.karumbi@andela.com>
2016-02-08 21:23:13 +0300

    Created test suite for class Librarian

---
+++ test/com/andela/bookclub/operations/LibrarianTest.java
@@ -0,0 +1,23 @@
+package com.andela.bookclub.operations;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LibrarianTest {
+
+    @Test
+    public void testRegisterNewBook() throws Exception {
+
+    }
+
+    @Test
+    public void testAddBookRequest() throws Exception {
+
+    }
+
+    @Test
+    public void testLendBooks() throws Exception {
+
+    }
+}

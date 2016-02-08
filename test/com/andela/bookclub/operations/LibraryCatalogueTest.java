ce7f0d26f35fc2d16d47a13a65fbd7772d924c4a
Eston Karumbi <eston.karumbi@andela.com>
2016-02-08 08:37:23 +0300

    Created test suite for class LibraryCatalogue

---
+++ test/com/andela/bookclub/operations/LibraryCatalogueTest.java
@@ -0,0 +1,28 @@
+package com.andela.bookclub.operations;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LibraryCatalogueTest {
+
+    @Test
+    public void testAddNewBook() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateBookDetails() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteBook() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBookByIsbn() throws Exception {
+
+    }
+}

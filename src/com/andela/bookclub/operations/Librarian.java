c42b3dd6de0665c3705d9cf76c335b3c5f422924
Eston Karumbi <eston.karumbi@andela.com>
2016-02-14 14:44:28 +0300

    Added method registerNewMember to class Librarian

--- src/com/andela/bookclub/operations/Librarian.java
+++ src/com/andela/bookclub/operations/Librarian.java
@@ -11,6 +11,8 @@ public class Librarian {
 
     private LibraryCatalogue catalogue;
 
+    private LibraryMembers members;
+
     private Map<Book, Queue<BookRequest>> staffBookQueueMap;
 
     private Map<Book, Queue<BookRequest>> studentBookQueueMap;
@@ -29,6 +31,18 @@ public class Librarian {
         this.staffBookQueueMap = new HashMap<>();
         this.studentBookQueueMap = new HashMap<>();
         this.catalogue = new LibraryCatalogue();
+        this.members = new LibraryMembers();
+    }
+
+    /**
+     * Registers a new member and returns true for a successful operation,
+     * or false otherwise.
+     * @param member The member to be added
+     * @return true if successful, false otherwise
+     * */
+
+    public boolean registerNewMember(Member member) {
+        return members.addNewMember(member);
     }
 
     /**

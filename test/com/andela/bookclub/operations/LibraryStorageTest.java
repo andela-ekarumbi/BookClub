08b7ab0ce82702fb7d37720dc8671eb40f928bee
Eston Karumbi <eston.karumbi@andela.com>
2016-02-22 20:12:06 +0300

    Create test suite for class LibraryStorage

---
+++ test/com/andela/bookclub/operations/LibraryStorageTest.java
@@ -0,0 +1,38 @@
+package com.andela.bookclub.operations;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LibraryStorageTest {
+
+    @Test
+    public void testAddItem() throws Exception {
+
+    }
+
+    @Test
+    public void testAddItemCollection() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAllItems() throws Exception {
+
+    }
+
+    @Test
+    public void testGetItemByUniqueId() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateItemDetails() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteItem() throws Exception {
+
+    }
+}
